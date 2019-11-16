f5b33335dcbd76976d10181493487ee741f6ef16
Ds767 <15981920968@163.com>
2019-11-16 23:05:49 +0800

    Download test

--- MainScript.js
+++ MainScript.js
@@ -2,7 +2,9 @@ var IS=document.getElementById("InputSize");
 var SS=document.getElementById("Screenshot");
 var ctx=SS.getContext("2d");
 var img=new Image();
+img.setAttribute('crossOrigin', 'anonymous');
 var img2=new Image();
+img2.setAttribute('crossOrigin', 'anonymous');
 var imgw=101; //image width Lock
 var imgh=257; //image height 30'score
 var imgitv=141; //interval of image to image
@@ -10,10 +12,13 @@ var imgitv1=20; //interval of image inside
 function Generate(){
 	SS.width=document.getElementById("InputWidth").value;
 	SS.height=document.getElementById("InputHeight").value;
+	ctx.fillStyle="#ffffff";
+	ctx.fillRect(0,0,SS.width,SS.height);
 	DrawBG(); //draw background
 	DrawPG(); //draw progress bar and score text and data
 	//DrawTX(); //draw text of score detail.time,score,item,time
 	//Draw2();
+	
 }
 
 function DrawBG(){
@@ -34,6 +39,7 @@ function DrawPG(){ //存在两次提交不一样成绩的图片的情况！！�
 		rm=Math.floor(Math.random()*20);
 		ctx.drawImage(img,ax-imgitv*i,ay-imgh/30*rm,imgw,imgh/30*(30+rm));
 		ctx.fillText(30+rm+"分",ax-imgitv*i+20,ay-imgh/30*rm-20);
+		//ctx.fillText();日期
 	}
 	ctx.fillStyle="#ff0000";
 	ctx.font="35px Georgia";
@@ -46,3 +52,11 @@ function DrawPG(){ //存在两次提交不一样成绩的图片的情况！！�
 function DrawTX(){
 
 }
+
+function Download(){
+	var imageObj=new Image();
+	imageObj.setAttribute('crossOrigin', 'anonymous');
+	imageObj.src=SS.toDataURL("image/png");
+	window.open(SS.toDataURL("image/png"));
+}
+
