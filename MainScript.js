850d386e34337f9e98651fcf502b01e50d19519a
Ds767 <15981920968@163.com>
2019-11-16 21:48:58 +0800

    progress done

--- MainScript.js
+++ MainScript.js
@@ -1,30 +1,48 @@
 var IS=document.getElementById("InputSize");
 var SS=document.getElementById("Screenshot");
-var cxt=SS.getContext("2d");
+var ctx=SS.getContext("2d");
 var img=new Image();
-var ax=0; //X Axis
-var ay=0; //Y Axis
-var imgw=20; //image width
-var imgh=20; //image height
+var img2=new Image();
+var imgw=101; //image width Lock
+var imgh=257; //image height 30'score
+var imgitv=141; //interval of image to image
+var imgitv1=20; //interval of image inside
 function Generate(){
 	SS.width=document.getElementById("InputWidth").value;
 	SS.height=document.getElementById("InputHeight").value;
-	Draw1();
+	DrawBG(); //draw background
+	DrawPG(); //draw progress bar and score text and data
+	//DrawTX(); //draw text of score detail.time,score,item,time
 	//Draw2();
 }
 
-function Draw1(){
-	img.src="Res/qg1.png";
-	cxt.drawImage(img,ax,ay,imgw,imgh); //Left 1
-	cxt.drawImage(img,ax+5,ay,imgw,imgh); //Left 2
-	cxt.drawImage(img,ax+10,ay,imgw,imgh); //Left 3
-	cxt.drawImage(img,ax+15,ay,imgw,imgh); //Left 4
-	cxt.drawImage(img,ax+20,ay,imgw,imgh); //Left 5
-	cxt.drawImage(img,ax+25,ay,imgw,imgh); //Left 6
-	cxt.drawImage(img,ax+30,ay,imgw,imgh); //Left 7
+function DrawBG(){
+	ctx.fillStyle="#fafafa";
+	ctx.fillRect(0,160,SS.width,imgh/3*8);
 }
 
-function Drwa2(){
-	img.src="";
-	cxt.drawImage(img,ax,ay,imgw,imgh); //Right 1
+function DrawPG(){ //存在两次提交不一样成绩的图片的情况！！！
+	
+	var ax=SS.width+imgitv1; //left corner offset
+	var ay=160+imgh/3*5; //30'score y axis
+	var rm=0; //random number
+	
+	ctx.fillStyle="#cccccc";
+	ctx.font="30px Georgia";
+	img.src="Res/qg1.png";
+	for(var i=2;i<9;++i){ //Left i
+		rm=Math.floor(Math.random()*20);
+		ctx.drawImage(img,ax-imgitv*i,ay-imgh/30*rm,imgw,imgh/30*(30+rm));
+		ctx.fillText(30+rm+"分",ax-imgitv*i+20,ay-imgh/30*rm-20);
+	}
+	ctx.fillStyle="#ff0000";
+	ctx.font="35px Georgia";
+	img2.src="Res/qg2.png";
+	rm=Math.floor(Math.random()*20);
+	ctx.drawImage(img2,ax-imgitv*1,ay-imgh/30*rm,imgw,imgh/30*(30+rm)); //Right 1
+	ctx.fillText(30+rm+"分",ax-imgitv*1+18,ay-imgh/30*rm-22);
+}
+
+function DrawTX(){
+
 }
